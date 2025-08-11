3fcaa76194ef7156731ebdfbe2bc74043d61a805
Jeongwan Kim <krwd1117@gmail.com>
2025-08-11 17:03:10 +0900

    Spring 프로젝트 생성

---
+++ src/main/java/com/krwd/gudigunae/GudigunaeApplication.java
@@ -0,0 +1,13 @@
+package com.krwd.gudigunae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GudigunaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GudigunaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krwd/gudigunae/GudigunaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krwd.gudigunae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GudigunaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
